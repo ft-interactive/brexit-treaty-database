75207f0c108f28159c3f493a31e816331cd854c3
tomgp <tom.g.pearson@gmail.com>
2017-05-26 12:04:25 +0100

    typo in a comment

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ document.querySelector('input#tableFilter')
   .onkeyup = (event)=>{
     const filter = event.target.value.toUpperCase(); //get the contents of the search box in UPPERCASE
     document.querySelectorAll('#treatyTable tbody tr').forEach( (row)=>{ //for each selected row
-      const searchableText = row.textContent.toUpperCase(); //get tall the text content in UPPERCASE
+      const searchableText = row.textContent.toUpperCase(); //get all the text content in UPPERCASE
       if(searchableText.indexOf(filter) > -1){  //if the filter is in the searchableText of the row then carry on displaying the row
         row.style.display = '';
       }else{  //otherwise hide it
