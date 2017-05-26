0228486a01f6929a8e2f0bea7ea4c03f4c203055
tomgp <tom.g.pearson@gmail.com>
2017-05-26 11:52:16 +0100

    make the javascript built

---
+++ client/index.js
@@ -0,0 +1,12 @@
+document.querySelector('input#tableFilter')
+  .onkeyup = (event)=>{
+    const filter = event.target.value.toUpperCase(); //get the contents of the search box in UPPERCASE
+    document.querySelectorAll('#treatyTable tbody tr').forEach( (row)=>{ //for each selected row
+      const searchableText = row.textContent.toUpperCase(); //get tall the text content in UPPERCASE
+      if(searchableText.indexOf(filter) > -1){  //if the filter is in the searchableText of the row then carry on displaying the row
+        row.style.display = '';
+      }else{  //otherwise hide it
+        row.style.display = 'none';
+      }
+    });
+  };
